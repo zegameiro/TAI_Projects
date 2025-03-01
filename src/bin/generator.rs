fe6a4dcc27ab82675cf42dfb5e4bba74c59ea34f
GuiAmorim03 <guilhermeamorim@ua.pt>
2025-03-01 22:46:49 +0000

    generator creates its own model

--- src/bin/generator.rs
+++ src/bin/generator.rs
@@ -1,10 +1,11 @@
 extern crate argparse;
 extern crate rand;
 
-use tai_first_project::{model_saver_loader::load_model, text_generator};
+use tai_first_project::{finite_context_model::FiniteContextModel, model_saver_loader::load_model, text_generator, *};
 use argparse::{ArgumentParser, Store};
 
 fn main() {
+    let mut file_path: String = "".to_string();
     let mut k_value: usize = 3;
     let mut alpha: f64 = 0.01;
     let mut prior: String = String::new();
@@ -14,6 +15,11 @@ fn main() {
         let mut argument_parser: ArgumentParser<'_> = ArgumentParser::new();
         argument_parser.set_description("Algorithmic Theory of Information First Project");
 
+        // File path
+        argument_parser.refer(&mut file_path)
+        .add_argument("File Path", Store, "Path to the file to be analysed")
+        .required();
+    
         // Size of the sliding window - k
         argument_parser.refer(&mut k_value)
             .add_option(&["-k"], Store, "Size of the sliding window");
@@ -33,15 +39,41 @@ fn main() {
         argument_parser.parse_args_or_exit();
     }
 
+    let mut file_reader_struct = file_reader::FileReader{
+        filename:String::from(file_path),
+        reader:Option::None,
+        buffer:String::new(),
+    };
+
+    if !file_reader::open_file(&mut file_reader_struct).is_ok(){
+        println!("error ReadingFile");
+        return;
+    }
+
+    let mut model = FiniteContextModel::new(k_value, alpha);
+
+    println!("Training model...");
+
+    loop {
+        match file_reader::read_char(&mut file_reader_struct) {
+            Ok(Some(char)) => {
+                model.train_char(char);
+            }
+            Ok(None) => break,
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+                break;
+            }
+        }
+    }
+
+
     // if prior.len() > k_value {
     //     eprint!("Prior sequence length must be less than or equal to k");
     //     return;
     // }
-    println!("Loading model...");
 
-    let model = load_model("modelSaved.model");
-
-    println!("Model Loaded successfully\nGenerating text...");
+    println!("Model created successfully\nGenerating text...");
     let generated_text = text_generator::generate_text(&model, &prior, sequence_length);
     println!("Generated Text:\n{}", generated_text);
 
