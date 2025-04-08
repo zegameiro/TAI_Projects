8c01e58629cb62864fd9cb47875aeccdeaf0d7ad
GuiAmorim03 <guilhermeamorim@ua.pt>
2025-04-08 22:56:05 +0100

    new meta generations and db_test

--- gto/generate_meta.sh
+++ gto/generate_meta.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
-output_random="data/meta_random.txt"
-mkdir -p "$(dirname "$output_random")"
-> "$output_random"
+mkdir -p data
 
-output_mutate="data/meta_mutate.txt"
-mkdir -p "$(dirname "$output_mutate")"
-> "$output_mutate"
+output_db="data/db_test.txt"
+> "$output_db"
 
-max=11310
+num_db_samples=50
 
-for s in $(seq 1 $max); do
+# db_test.txt
+for i in $(seq 1 $num_db_samples); do
+    # 1000 - 10000
+    len=$((RANDOM % 9001 + 1000))
 
-    sequence=$(gto_genomic_gen_random_dna -s "$s" -n 151)
+    sequence=$(gto_genomic_gen_random_dna -s "$i" -n "$len")
 
-    echo "$sequence" >> "$output_random"
+    echo "@seq_$i" >> "$output_db"
 
-    printf "%s\n" "$sequence" | gto_genomic_dna_mutate -s "$s" -m 0.5 | tr -d '\n' >> "$output_mutate"
-    echo "" >> "$output_mutate"
+    echo "$sequence" | fold -w 75 >> "$output_db"
+done
 
-    percent=$(awk "BEGIN {printf \"%.2f\", ($s / $max) * 100}")
-    echo -ne "$percent% \r"
+
+# meta_n.txt
+max=113
+for n in $(seq 1 5); do
+    output_mutate="data/meta_${n}.txt"
+    mkdir -p "$(dirname "$output_mutate")"
+    > "$output_mutate"
+
+
+    for s in $(seq 1 $max); do
+
+        seq_id=$((RANDOM % n + 1))
+
+        seq=$(awk -v id="@seq_$seq_id" '
+            $0 ~ id {flag=1; next}
+            /^@/ {flag=0}
+            flag {printf "%s", $0}
+        ' data/db_test.txt)
+
+        sequence=$(echo "$seq" | cut -c1-151)
+
+
+        printf "%s\n" "$sequence" | gto_genomic_dna_mutate -s "$s" -m 0.5 | tr -d '\n' >> "$output_mutate"
+        echo "" >> "$output_mutate"
+
+        percent=$(awk -v n="$n" -v s="$s" -v max="$max" '
+            BEGIN { printf "%.2f", ((n - 1 + s / max) / 5) * 100 }
+        ')
+        echo -ne "$percent% \r"
+    done
 done
 
